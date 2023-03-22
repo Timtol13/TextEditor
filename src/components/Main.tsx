a0305cf159b7fda939ed7ba3f243e45a00f8d28a
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-03-22 20:05:43 +0300

    Add task append, display tasks

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import './Main.scss'
+import {Add, Data} from "./add/Add";
 
 export const Main = () => {
     return (
         <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/add'} element={<Add />} />
+                    <Route path={'/'} element={<Tasks />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
 
+export const Tasks = () => {
+    const title = localStorage.getItem('title')
+    const [tasks, setTasks] = useState<Data[]>()
+    useEffect(() => {
+        setTasks(title? JSON.parse(title).reverse() : [])
+    }, [])
+    return (
+        <div>
+            {tasks?.map(task => {
+                return (
+                    <div className={'task'}>
+                        <div className={'title'}>{task.title}</div>
+                        <div className={'description'}>{task.description}</div>
+                        <div className={'date'}>{task.date}</div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
---
+++ src/components/add/Add.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import {useFormik} from "formik";
+import './Add.scss'
+import {useNavigate} from "react-router-dom";
+
+export type Data = {
+    title: string,
+    description: string,
+    date: string
+}
+
+export const Add = () => {
+    let date = new Date()
+    const nav = useNavigate()
+    let [allData, setData] = useState<Data[]>([])
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: '',
+            date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+        },
+        onSubmit: (values: Data) => {
+            let title = localStorage.getItem('title')
+            let value: Data
+            value = values
+            allData = title? JSON.parse(title) : []
+            const newData = [...allData, value];
+            localStorage.setItem('title', JSON.stringify(newData))
+            return nav('/')
+        }
+    })
+    return (
+        <div className={'add'}>
+            <form onSubmit={formik.handleSubmit}>
+                <input type={"text"} className={'element title'} placeholder={"Enter title"} {...formik.getFieldProps('title')} />
+                <textarea placeholder={"Enter description"} className={'element description'} {...formik.getFieldProps('description')} ></textarea>
+                <button type={"submit"} className={'element btn'} >Submit</button>
+            </form>
+        </div>
+    )
+}
