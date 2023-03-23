017d8ec5afc18b2b121e57bb4c2fd2219083386f
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-03-23 10:55:15 +0300

    Search tasks, filter by tags, delete tags and change tags

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,19 +19,88 @@ export const Main = () => {
 export const Tasks = () => {
     const title = localStorage.getItem('title')
     const [tasks, setTasks] = useState<Data[]>()
+    const [search, setSearch] = useState<string>()
+    const [tags, setTags] = useState<string[]>([])
     useEffect(() => {
         setTasks(title? JSON.parse(title).reverse() : [])
     }, [])
+    console.log(tasks)
+    tasks?.map(task => {
+        if(task?.tag !in tags) {
+            setTags(e => [...e, task.tag])
+        }
+    })
+    console.log(tags)
     return (
         <div>
+            <div className={'tags'}>
             {tasks?.map(task => {
+                if (task.tag) {
+                    if (task?.tag ! in tags) {
+                        setTags(e => [...e, task.tag])
+                    }
+                    return (
+                        <div className={'btns'}>
+                            <button
+                                className={'btn'}
+                                onClick={() => {setSearch(task.tag)}}>
+                                {task.tag}
+                            </button>
+                            <button className={'btn w10'} onClick={() => {
+                                tasks?.map(taskDel => {
+                                    if(taskDel.tag === task.tag){
+                                        return taskDel.tag = ""
+                                    }
+                                })
+                                let value: Data[]
+                                value = tasks
+                                console.log(value)
+                                localStorage.setItem('title', JSON.stringify(value))
+                                }
+                            }>X</button>
+                        </div>
+                    )
+                }
+            })}
+            </div>
+            <div className={'search_add'}>
+                <input placeholder={'search by tag'} id={'search'} value={search} className={'search'} onChange={val => {setSearch(val.target.value)}} />
+                <a type={'button'} className={'btn w10 green'} href={'/add'}>+</a>
+            </div>
+            {!search && tasks?.map(task => {
                 return (
                     <div className={'task'}>
+                        <div className={'tag'}>
+                            Tag: <input type={'text'} placeholder={task.tag ? task.tag : 'empty'} onChange={e => {
+                                task.tag = e.target.value
+                                localStorage.setItem('title', JSON.stringify(tasks))
+                            }
+                            } />
+                        </div>
                         <div className={'title'}>{task.title}</div>
-                        <div className={'description'}>{task.description}</div>
+                        <div className={'description'}><p>{task.description}</p></div>
                         <div className={'date'}>{task.date}</div>
                     </div>
                 )
+            })
+            }
+            {search && tasks?.map(task => {
+                if (task.tag === search){
+                    return (
+                        <div className={'task'}>
+                            <div className={'tag'}>
+                                Tag: <input type={'text'} placeholder={task.tag ? task.tag : 'empty'} onChange={e => {
+                                task.tag = e.target.value
+                                localStorage.setItem('title', JSON.stringify(tasks))
+                            }
+                            } />
+                            </div>
+                            <div className={'title'}>{task.title}</div>
+                            <div className={'description'}>{task.description}</div>
+                            <div className={'date'}>{task.date}</div>
+                        </div>
+                    )
+                }
             })}
         </div>
     )
--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -4,6 +4,7 @@ import './Add.scss'
 import {useNavigate} from "react-router-dom";
 
 export type Data = {
+    tag: string,
     title: string,
     description: string,
     date: string
@@ -15,6 +16,7 @@ export const Add = () => {
     let [allData, setData] = useState<Data[]>([])
     const formik = useFormik({
         initialValues: {
+            tag: '',
             title: '',
             description: '',
             date: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
@@ -32,6 +34,7 @@ export const Add = () => {
     return (
         <div className={'add'}>
             <form onSubmit={formik.handleSubmit}>
+                <input placeholder={'Tag'} maxLength={15} className={'element tag'} {...formik.getFieldProps('tag')} />
                 <input type={"text"} className={'element title'} placeholder={"Enter title"} {...formik.getFieldProps('title')} />
                 <textarea placeholder={"Enter description"} className={'element description'} {...formik.getFieldProps('description')} ></textarea>
                 <button type={"submit"} className={'element btn'} >Submit</button>
